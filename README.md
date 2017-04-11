c8ff06971dbbb451656207330ba39be15a331cea
Chuck Wooters <wooters@semanticmachines.com>
2017-04-11 12:10:34 -0700

    rework and readme

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # codalab_bash
-A few useful codalab bash commands
+A couple of useful codalab bash commands.
+
+## `cl_bookmark`
+This command will save info about the currently-active worksheet.
+
+## `cl_workhere`
+This command will set the currently-active worksheet to the one specified by a previous `cl_bookmark` command.
+
+These commands us a hidden "dot-file" (`.cl_worksheet` by default) in the local dir to record the bookmark info.
+
+## Installation
+Clone this repo locally and add something like the following to your `.bashrc`:
+
+``` bash
+if [ -f "${HOME}/projects/codalab_bash/codalab_bash.sh" ]; then
+    source "${HOME}/projects/codalab_bash/codalab_bash.sh"
+fi
+```
+
+## Integration with (bash-git-prompt)[https://github.com/magicmonty/bash-git-prompt]
+If you are using (bash-git-prompt)[https://github.com/magicmonty/bash-git-prompt] you can include
+information about the currently-bookmarked worksheet into the prompt. This is done using the
+`prompt_callback` function of `bash-git-prompt`. For example, if you have installed `bash-git-prompt`
+via homebrew:
+
+``` bash
+if [ -f "${HOME}/projects/codalab_bash/codalab_bash.sh" ]; then
+    source "${HOME}/projects/codalab_bash/codalab_bash.sh"
+fi
+
+if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+    
+    function prompt_callback {
+        local cl_prompt
+        if [ -z ${HAVE_CL_BASH+x} ]; then
+            echo ""
+        else
+            if ! cl_prompt=$(CL_PROMPT_INFO); then
+                echo ""
+            else
+                if ! CL_WS_IS_CONSISTENT; then
+                    echoc " [${BoldRed}${cl_prompt}${ResetColor}]"
+                else
+                    echo " [${cl_prompt}]"
+                fi
+            fi
+        fi
+    }
+    
+    # For customization info see: https://github.com/magicmonty/bash-git-prompt
+    __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+    source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+fi
+```
+
--- codalab_bash.sh
+++ codalab_bash.sh
@@ -1,37 +1,27 @@
 #!/usr/bin/env bash
 
+export HAVE_CL_BASH=1
+
 # Use a dot-file to associate the current dir
 # with a specific codalab worksheet
-CL_LOCAL_WS_INFO_FILE=".cl_worksheet"
+CL_LOCAL_WS_INFO_FILE="${CL_LOCAL_WS_INFO_FILE:-.cl_worksheet}"
 
 # The location of the codalab state file
-__CL_STATE_FILE="${HOME}/.codalab/state.json"
+CL_STATE_FILE="${CL_STATE_FILE:-${HOME}/.codalab/state.json}"
 
 # A regex for parsing the worksheet name and uuid
-__CL_WS_REGEX=".+:[0-9]+::(.+)\((0x[a-f0-9]+)\)"
+# from a string of the form:
+#    ::name(uuid)
+# as returned by the `cl work` command
+__CL_WS_REGEX="::(.+)\((0x[a-f0-9]+)\)"
 
-function CL_LOGGED_IN {
-    # returns success (0) if user is logged in, failure (1) if not
-    if [[ -f ${__CL_STATE_FILE} ]]; then
-        if ! res=$(python -c "import sys, json; print(len(json.load(sys.stdin)['auth']))" < "${__CL_STATE_FILE}"); then
-            return $?
-        fi
-        if [[ $res == "0" ]]; then
-            return 1
-        fi
-        return 0
-    fi
-    return 1
-}
-
-function CL_STATE_CURRENT_WS_UUID {
-    # Get current worksheet according to the state.json file
-    python -c "import sys, json; print(json.load(sys.stdin)['sessions']['top']['worksheet_uuid'])" < "${__CL_STATE_FILE}"
-}
+__CL_SUCCESS=0
+__CL_FAILURE=1
 
 function __CL_PARSE_WS_UUID {
     # gets the UUID of the passed in worksheet
-    local out=""
+    local out
+    out=""
     if [[ $1 =~ $__CL_WS_REGEX ]]; then
         out="${BASH_REMATCH[2]}"
     fi
@@ -40,70 +30,174 @@ function __CL_PARSE_WS_UUID {
 
 function __CL_PARSE_WS_NAME {
     # gets the name of the passed in worksheet
-    local out=""
+    local out
+    out=""
     if [[ $1 =~ $__CL_WS_REGEX ]]; then
         out="${BASH_REMATCH[1]}"
     fi
     echo "$out"
 }
 
-function CL_GET_LOCAL_WS_UUID {
-    __CL_PARSE_WS_UUID "$(cat $CL_LOCAL_WS_INFO_FILE)"
-}
-
-function CL_GET_LOCAL_WS_NAME {
-    __CL_PARSE_WS_NAME "$(cat $CL_LOCAL_WS_INFO_FILE)"
-}
-
-function CL_AVAILABLE {
-    # returns true if codalab cli available in PATH
-    # and user is logged in
-    which cl > /dev/null 2>&1 && CL_LOGGED_IN
-}
-
 function __RUN_CL_WORK {
-    # If no arg passed in, just reports current worksheet
-    # Using `expect` to look for the case where the user
-    # is not currently logged in
-    if ! cl_work_out=$(CL_AVAILABLE && expect -c "spawn -noecho cl work $1; expect \"Username:\" {exit 1}"); then
+    # If no arg passed in, just returns the current worksheet
+    local cl_work_out
+    if ! cl_work_out=$(CL_AVAILABLE && cl work "$1"); then
         echo "Error running the 'cl work' command. Is the codalab cli in your PATH and are you logged in?" >&2
         echo ""
-        return $?
+        return $__CL_FAILURE
     else
-        echo "$cl_work_out"
-        return 0
+        echo "${cl_work_out}"
     fi
 }
 
+function CL_LOGGED_IN {
+    # returns success (0) if user is logged in, failure (1) if not
+    local res
+    if [[ -f "${CL_STATE_FILE}" ]]; then
+        if ! res=$(python -c "import sys, json; print(len(json.load(sys.stdin)['auth']))" < "${CL_STATE_FILE}"); then
+            return $?
+        fi
+        if [[ $res == "0" ]]; then
+            return $__CL_FAILURE
+        fi
+        return $__CL_SUCCESS
+    fi
+    return $__CL_FAILURE
+}
+
+function CL_AVAILABLE {
+    # returns success if codalab cli available in PATH
+    # and user is logged in
+    CL_LOGGED_IN && which cl > /dev/null 2>&1
+}
+
+function CL_GET_LOCAL_WS_INFO {
+    if [[ -f "${CL_LOCAL_WS_INFO_FILE}" ]]; then
+        cat "${CL_LOCAL_WS_INFO_FILE}"
+    else
+        echo ""
+        return $__CL_FAILURE
+    fi
+}
+
+function CL_GET_LOCAL_WS_UUID {
+    local ws_info
+    if ws_info=$(CL_GET_LOCAL_WS_INFO); then
+        __CL_PARSE_WS_UUID "${ws_info}"
+    else
+        echo ""
+        return $__CL_FAILURE
+    fi
+}
+
+function CL_GET_LOCAL_WS_NAME {
+    local ws_info
+    if ws_info=$(CL_GET_LOCAL_WS_INFO); then
+        __CL_PARSE_WS_NAME "${ws_info}"
+    else
+        echo ""
+        return $__CL_FAILURE
+    fi
+}
+
+function CL_GET_STATE_WS_UUID {
+    # Get current worksheet UUID according to the state.json file
+    if [[ -f "${CL_STATE_FILE}" ]]; then
+        python -c "import sys, json; print(json.load(sys.stdin)['sessions']['top']['worksheet_uuid'])" < "${CL_STATE_FILE}"
+    else
+        echo ""
+        return $__CL_FAILURE
+    fi
+}
+
+function CL_GET_STATE_WS_SERVER {
+    # Get current server according to the state.json file
+    if [[ -f "${CL_STATE_FILE}" ]]; then
+        python -c "import sys, json; print(json.load(sys.stdin)['sessions']['top']['address'])" < "${CL_STATE_FILE}"
+    else
+        echo ""
+        return $__CL_FAILURE
+    fi
+}
+
+function CL_WS_IS_CONSISTENT {
+    # Returns success if:
+    #   1. The user is logged in to codalab
+    #   2. The worksheet listed in the local worksheet info file
+    #      is the same as the active worksheet in the codalab
+    #      state file
+    local state_ws_uuid
+    local local_ws_uuid
+    if CL_LOGGED_IN; then
+        if ! state_ws_uuid=$(CL_GET_STATE_WS_UUID); then
+            return $__CL_FAILURE
+        fi
+        if ! local_ws_uuid=$(CL_GET_LOCAL_WS_UUID); then
+            return $__CL_FAILURE
+        fi
+        [ "${state_ws_uuid}" == "${local_ws_uuid}" ]
+    else
+        return $__CL_FAILURE
+    fi
+}
+
+function CL_PROMPT_INFO {
+    # Returns a short string for the local worksheet
+    # suitable for display in a prompt
+    local ws_name
+    local ws_uuid
+    if [[ -f "${CL_LOCAL_WS_INFO_FILE}" ]]; then
+        if ! ws_name=$(CL_GET_LOCAL_WS_NAME); then
+            echo ""
+            return $__CL_FAILURE
+        fi
+        if ! ws_uuid=$(CL_GET_LOCAL_WS_UUID); then
+            echo ""
+            return $__CL_FAILURE
+        fi
+        echo "${ws_name}(${ws_uuid:0:8})"
+    else
+        echo ""
+        return $__CL_FAILURE
+    fi
+}
 
 #
 # Command line helper utilities
 #
+
 function cl_workhere {
     # changes the active codalab worksheet to be the same as
-    # what is in the cur dir (as recorded in $CL_LOCAL_WS_INFO_FILE)
-    if [[ -f $CL_LOCAL_WS_INFO_FILE ]]; then
-        ws_uuid=$(CL_GET_LOCAL_WS_UUID)
-        if [[ "$ws_uuid" == "" ]]; then
-            echo "Couldn't parse UUID from ${CL_LOCAL_WS_INFO_FILE}" >&2
-            return 1
+    # the one stored locally (in $CL_LOCAL_WS_INFO_FILE)
+    local ws_uuid
+    local ws_info
+    if [[ -f "${CL_LOCAL_WS_INFO_FILE}" ]]; then
+        if CL_WS_IS_CONSISTENT; then
+            return $__CL_SUCCESS
         fi
-        if ! __RUN_CL_WORK "$(CL_GET_LOCAL_WS_UUID)"; then
-            return $?
+        if ! ws_uuid=$(CL_GET_LOCAL_WS_UUID); then
+            echo "Couldn't get worksheet UUID from ${CL_LOCAL_WS_INFO_FILE}" >&2
+            return $__CL_FAILURE
         fi
+        if ! ws_info=$(__RUN_CL_WORK "${ws_uuid}"); then
+            return $__CL_FAILURE
+        fi
+        # save it locally so that we have the worksheet name
+        # and not just the uuid
+        echo "${ws_info}" > "${CL_LOCAL_WS_INFO_FILE}"
     else
-        echo "$CL_LOCAL_WS_INFO_FILE not found" >&2
-        return 1
+        echo "$CL_LOCAL_WS_INFO_FILE file not found." >&2
+        echo "run 'cl_bookmark' to record the current worksheet in this dir" >&2
+        return $__CL_FAILURE
     fi
 }
 
 function cl_bookmark {
     # saves the currently-active worksheet info into $CL_LOCAL_WS_INFO_FILE
+    local ws_info
     if ! ws_info=$(__RUN_CL_WORK); then
-        return $?
+        return $__CL_FAILURE
     fi
-    echo "Saving worksheet info to: $CL_LOCAL_WS_INFO_FILE"
-    echo "${ws_info}" > ${CL_LOCAL_WS_INFO_FILE}
-    return 0
+    echo "Saving active worksheet info to: $CL_LOCAL_WS_INFO_FILE"
+    echo "${ws_info}" > "${CL_LOCAL_WS_INFO_FILE}"
 }
-
